d78ba5bb7573849dab85656fe57eee8b7bde84f1
malaka96 <madhubhashana655@gmail.com>
2026-02-02 09:23:35 +0530

    initial commit

---
+++ src/main/java/edu/malaka96/authify_backend/AuthifyBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.malaka96.authify_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthifyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthifyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/malaka96/authify_backend/AuthifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.malaka96.authify_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthifyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
